459ca286ff6dd24f5d46b86e2b8c5905e9315ec3
Mai Đình Khôi <mdkh@netcompany.com>
2025-10-07 00:16:05 +0700

    Initial commit: bootstrap Spring Boot app with Java 17, PostgreSQL, and Liquibase

---
+++ src/test/java/com/netcompany/onboardingexcercise1/OnboardingExcercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcompany.onboardingexcercise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingExcercise1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
