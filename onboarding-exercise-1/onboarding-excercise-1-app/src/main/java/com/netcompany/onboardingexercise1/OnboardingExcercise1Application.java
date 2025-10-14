691fbbe5f6e30f1f66e05f82b3be6cdbbf489901
DinhKhoiMai <21521008@gm.uit.edu.vn>
2025-10-14 22:24:45 +0700

    fix: add missing main module to onboarding-exercise-1

---
+++ onboarding-exercise-1/onboarding-excercise-1-app/src/main/java/com/netcompany/onboardingexercise1/OnboardingExcercise1Application.java
@@ -0,0 +1,11 @@
+package com.netcompany.onboardingexercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages= {"com.netcompany.onboardingexercise1"})
+public class OnboardingExcercise1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingExcercise1Application.class, args);
+    }
+}
