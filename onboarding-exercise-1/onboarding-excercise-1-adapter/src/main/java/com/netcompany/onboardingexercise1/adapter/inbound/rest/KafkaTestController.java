5a2dadf7d0bcd3b73bc8ab1ba1ac5975f1b8e8ba
DinhKhoiMai <21521008@gm.uit.edu.vn>
2025-10-17 10:48:55 +0700

    feat: add batch producer for TaxCalculationEvent (testing purpose)

--- onboarding-exercise-1/onboarding-excercise-1-adapter/src/main/java/com/netcompany/onboardingexercise1/adapter/inbound/rest/KafkaTestController.java
+++ onboarding-exercise-1/onboarding-excercise-1-adapter/src/main/java/com/netcompany/onboardingexercise1/adapter/inbound/rest/KafkaTestController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netcompany.onboardingexercise1.event.taxcalculationevent.TaxCalculationEvent;
 import com.netcompany.onboardingexercise1.event.testevent.TestEvent;
+import com.netcompany.onboardingexercise1.shared.enums.ErrorCode;
+import com.netcompany.onboardingexercise1.shared.exception.UnexpectedException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
@@ -38,6 +40,29 @@ public class KafkaTestController {
         return "Event sent to topic: " + taxCalculationTopic;
     }
 
+    @PostMapping("/tax-calculations-events/batch")
+    public String sendTaxCalculationEvents(@RequestBody List<TaxCalculationEvent> taxCalculationEvents) {
+        if (taxCalculationEvents == null || taxCalculationEvents.isEmpty()) {
+            return "No taxCalculationEvents to send.";
+        }
+
+        log.info("Producing batch of {} TaxCalculationEvent to topic {}", taxCalculationEvents.size(), taxCalculationTopic);
+
+
+
+        List<CompletableFuture<SendResult<Integer, String>>> futures = taxCalculationEvents.stream().map(taxCalculationEvent -> {
+            try {
+                return kafkaTemplate.send(taxCalculationTopic, objectMapper.writeValueAsString(taxCalculationEvent)).toCompletableFuture();
+            } catch (JsonProcessingException e) {
+                throw new UnexpectedException(ErrorCode.ONBOARDING_UNEXPECTED_001, "Exception while producing kafka message", e);
+            }
+        }).toList();
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+
+        return "Sent " + taxCalculationEvents.size() + " taxCalculationEvents to topic: " + taxCalculationTopic;
+    }
+
     @PostMapping("/test-events/batch")
     public String sendTestEvents(@RequestBody List<TestEvent> testEvents) {
         if (testEvents == null || testEvents.isEmpty()) {
@@ -52,7 +77,7 @@ public class KafkaTestController {
             try {
                 return kafkaTemplate.send(testEventTopic, testEvent.getId().intValue(), objectMapper.writeValueAsString(testEvent)).toCompletableFuture();
             } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
+                throw new UnexpectedException(ErrorCode.ONBOARDING_UNEXPECTED_001, "Exception while producing kafka message", e);
             }
         }).toList();
 
